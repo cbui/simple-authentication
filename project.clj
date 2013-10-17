9788a9396f1d26892e3fd3344aff473ddad79bc0
Chris Bui <hesbornaliarhelldiealiar@gmail.com>
2013-10-17 15:50:47 -0500

    Add bcrypt password hashing and checking

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject simple-authentication "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple authentication for Ring web applications."
+  :url "https://github.com/Christopher-Bui/simple-authentication"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.mindrot/jbcrypt "0.3m"]])
---
+++ src/simple_authentication/bcrypt.clj
@@ -0,0 +1,15 @@
+(ns simple-authentication.bcrypt
+  (:import org.mindrot.jbcrypt.BCrypt))
+
+(defn hash-password
+  ([password]
+     "Hashes the password with BCrypt and a generated salt. The generated salt defaults to 10 log_rounds (How complex the salt is)."
+     (BCrypt/hashpw password (BCrypt/gensalt)))
+  ([password salt]
+     "Hashes the password with BCrypt and the given salt."
+     (BCrypt/hashpw password salt)))
+
+(defn check-password
+  [plain-text-password hashed-password]
+  "Checks the plain-text-password against the hashed-password. Returns true if they match and false if they don't."
+  (BCrypt/checkpw plain-text-password hashed-password))
